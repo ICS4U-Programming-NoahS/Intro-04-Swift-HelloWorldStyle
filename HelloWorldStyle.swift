e317278b8d11c3a85e509fbce985a828d44d715e
Noah Smith <noah.smith@stu.ocsb.ca>
2025-02-06 16:24:16 +0000

    HelloWorldStyle.swift

---
+++ HelloWorldStyle.swift
@@ -0,0 +1,11 @@
+//
+// HelloWorld.swift
+//
+// Created by Noah Smith
+// Created on 2025-02-06
+// Version 1.0
+// Copyright (c) 2025 Noah Smith. All rights reserved.
+//
+// Greeting my brother and asking him about school
+print("Hi Ben!")
+print("How is school going?")
